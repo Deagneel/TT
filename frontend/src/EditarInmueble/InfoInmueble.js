27745c45a8c765323cdd355faa267b7846a4de0f
Angel Tellez <angeltellezr26@gmail.com>
2023-12-06 02:59:42 -0600

    b

--- frontend/src/EditarInmueble/InfoInmueble.js
+++ frontend/src/EditarInmueble/InfoInmueble.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './EditInmue.css';
-import { faEnvelope, faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faExclamationTriangle, faHouse } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
 import axios from 'axios';
 
@@ -59,111 +59,92 @@ const handleReportClick = (idUsuario, idInmueble) => {
   }
 
   return (
-    <div className="app-container">
+    <div className="app-container text-center">
       {registeredProperties.map((property, index) => (
-    <div style={{ marginLeft: '25px', marginTop: '25px'}}>
-      <button onClick={handleRegresarClick} className="close-button">X</button>
-      <div style={{ marginBottom: '20px', display: 'flex', alignItems: 'flex-start' }}>
-        <div style={{ marginRight: '45px' }}>
-          <p style={{ marginBottom: '0' , fontWeight: 'bold'}}>Me interesa</p>
-          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-            <button onClick={handleInteresClick} className="color-icon" style={{ marginLeft:'24px' }}>
-              <FontAwesomeIcon icon={faEnvelope} style={{ fontSize: '30px', marginRight: '10px', cursor: 'pointer' }} />
-            </button>
+      <div key={index} style={{ width: '50%', padding: '10px', borderRadius: '8px' }} className="bg-light mx-auto">
+          <button onClick={handleRegresarClick} className="btn btn-danger close-button">X</button>
+
+          <div className="align-center mb-4">
+            <h2 className="font-weight-bold">{property.titulo}</h2>
+          </div>
+  
+          {/* Carrusel de imágenes */}
+          <div className="border mb-4" style={{ width: '80%', height: '50%', margin: '0 auto' }}>
+            <img src={`http://localhost:3031/images/${property.foto}`} alt="Imagen" className="img-fluid" />
+          </div>
+
+  
+          {/* Detalles del departamento */}
+          <div className="mb-4">
+            {/* Direccion */}
+            <div className="mb-3">
+              <h5 className="font-weight-bold">Dirección</h5>
+              <textarea readOnly value={property.direccion} placeholder="Ingresar reglamento" className="fondo-placeholder form-control" style={{ height: '130px' }} />
+            </div>
+            {/* Coordenadas */}
+            <div className="mb-3">
+              <h5 className="font-weight-bold">Coordenadas</h5>
+              <p className="fondo-placeholder" type="text">{property.coordenadas}</p>
+            </div>
+            {/* Num cuartos */}
+            <div className="mb-3">
+              <h5 className="font-weight-bold">Número de cuartos</h5>
+              <p className="fondo-placeholder" type="text">{property.no_habitaciones}</p>
+            </div>
+            {/* Precio */}
+            <div className="mb-3">
+              <h5 className="font-weight-bold">Precio</h5>
+              <p className="fondo-placeholder" type="text">{property.precio}</p>
+            </div>
+            {/* Periodo del contrato */}
+            <div className="mb-3">
+              <h5 className="font-weight-bold">Periodo del contrato</h5>
+              <p className="fondo-placeholder" type="text">{property.periodo_de_renta}</p>
+            </div>
+            {/* Tipo de habitación */}
+            <div className="mb-3">
+              <h5 className="font-weight-bold">Tipo de vivienda</h5>
+              <p className="fondo-placeholder" type="text">{tipoVivienda}</p>
+            </div>
+          </div>
+  
+          <div className="mb-4">
+            {/* Reglamento */}
+            <div className="mb-3" style={{ width: '100%', margin: '0 auto' }}>
+              <h5 className="mb-3 font-weight-bold">Reglamento</h5>
+              <textarea readOnly value={property.reglamento} placeholder="Ingresar reglamento" className="fondo-placeholder form-control" style={{ height: '300px' }} />
+            </div>
+          </div>
+          <div className="mb-4 d-flex justify-content-center">
+            {/* Botón "Me interesa" */}
+            <div className="me-3">
+              <p className="mb-0 font-weight-bold">Me interesa</p>
+              <button onClick={handleInteresClick} className="btn btn-primary">
+                <FontAwesomeIcon icon={faEnvelope} className="me-2" />
+              </button>
+            </div>
+
+            {/* Botón "Reportar" */}
+            <div className="me-3">
+              <p className="mb-0 font-weight-bold">Reportar</p>
+              <button onClick={() => handleReportClick(property.id_usuario, property.id_inmueble)} className="btn btn-warning">
+                <FontAwesomeIcon icon={faExclamationTriangle} className="me-2" />
+              </button>
+            </div>
+
+            {/* Botón "Trato" */}
+            <div className="me-3">
+              <p className="mb-0 font-weight-bold">Hacer Trato</p>
+              <button onClick={() => handleReportClick(property.id_usuario, property.id_inmueble)} className="btn btn-primary">
+                <FontAwesomeIcon icon={faHouse} className="me-2" />
+              </button>
+            </div>
           </div>
         </div>
-
-        <div style={{ marginRight: '45px' }}>
-          <p style={{ marginBottom: '0' , fontWeight: 'bold'}}>Reportar</p>
-          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-          <button onClick={() => handleReportClick(property.id_usuario, property.id_inmueble)} className="color-icon" style={{ marginLeft:'14px' }}>
-              <FontAwesomeIcon icon={faExclamationTriangle} style={{ fontSize: '30px',marginRight: '10px', cursor: 'pointer' }} />
-            </button>
-          </div>
-        </div>
-
-        {/* Placeholder titulo */}
-        <div className="align-center">
-        <span className='fondo-placeholder'
-          style={{fontWeight: 'bold', fontSize: '22px', maxWidth: '400px', marginLeft: '260px' }}
-        >
-            {property.titulo}</span>
-        </div>
-      </div>
-
-      {/* Carrusel de imágenes */}
-      <div style={{ marginLeft: '350px', marginTop: '1px', border: '1px solid #000', width: '550px', height: '270px', margin: '0 auto' }}>
-        <img src={'http://localhost:3031/images/'+ property.foto }  alt="Imagen" style={{ width: '550px', height: '270px'}} />
-      </div>
-
-      {/* Aquí comienzan los detalles del departamento */}
-      <div style={{ marginTop:'29px', marginBottom: '20px', display: 'flex', alignItems: 'flex-start' }}>
-      {/* Direccion */}
-      <div style={{ marginRight: '30px' }}>
-          <span style={{ marginBottom: '0', fontWeight: 'bold' }}>Dirección</span>
-          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-          <textarea readOnly value={property.direccion} placeholder="Ingresar reglamento" className='fondo-placeholder' 
-          style={{fontSize: '17px', width: '460px', height: '130px', resize: 'none' }} />
-          </div>
-        </div>
-      {/* Coordenadas */}
-      <div style={{ marginRight: '45px' }}>
-          <span style={{ marginBottom: '0' , fontWeight: 'bold'}}>Coordenadas</span>
-          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-          <p className='fondo-placeholder'  type="text" style={{ width: '215px', }} >{property.coordenadas}</p>
-          </div>
-          {/* Num cuartos */}
-      <div style={{ marginRight: '5px' }}>
-          <span style={{ marginBottom: '0' , fontWeight: 'bold'}}>Número de cuartos</span>
-          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-          <p  className='fondo-placeholder'  type="text" style={{ width: '40px', }} >{property.no_habitaciones}</p>
-          </div>
-      </div>
-      </div>
-      {/* Precio */}
-      <div style={{ marginRight: '60px' }}>
-          <span style={{ marginBottom: '0', fontWeight: 'bold' }}>Precio</span>
-          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-          <p className='fondo-placeholder'  type="text" style={{  width: '100px', }} >{property.precio}</p>
-          </div>
-      </div>
-      {/* Periodo del contrato */}
-      <div style={{ marginRight: '80px' }}>
-          <span style={{ marginBottom: '0', fontWeight: 'bold' }}>Periodo del contrato</span>
-          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-            <p className='fondo-placeholder'  type="text" style={{  width: '155px', }} >{property.periodo_de_renta}</p>
-          </div>
-      </div>
-      {/* Tipo de habitación */}
-      <div>
-          <span style={{ marginBottom: '0' , fontWeight: 'bold'}}>Tipo de vivienda</span>
-          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-            <p className='fondo-placeholder'  type="text" style={{  width: '125px', }} >
-              {tipoVivienda}
-            </p>
-          </div>
-      </div>
-      </div>
-
-      <div style={{ marginRight:'750px', marginTop:'29px', marginBottom: '20px', display: 'flex', alignItems: 'flex-start' }}>
-          {/* Reglamento */}
-          <div style={{ marginRight: '35px', width: '620px', margin: '0 auto' }}>
-            <span style={{marginLeft: '290px', marginBottom: '0', fontWeight: 'bold' }}>Reglamento</span>
-            <textarea readOnly value={property.reglamento} placeholder="Ingresar reglamento" className='fondo-placeholder' style={{fontSize: '17px', width: '675px', height: '500px', resize: 'none' }} />
-          </div>
-
-          <div style={{marginLeft:'200px' ,display: 'flex', flexDirection: 'column' }}>
-            <span style={{ font: 'inherit', whiteSpace: 'nowrap', fontWeight: 'bold' }}>Luis Alberto</span>
-            <span style={{ font: 'inherit', whiteSpace: 'nowrap' , fontWeight: 'bold'}}>Juan Pérez</span>
-            <span style={{ font: 'inherit', whiteSpace: 'nowrap' , fontWeight: 'bold'}}>María García</span>
-            <span style={{ font: 'inherit', whiteSpace: 'nowrap', fontWeight: 'bold' }}>Carlos González</span>
-          </div>
-      </div>
-      
-    </div>
-     ))}
+      ))}
     </div>
   );
+
 }
 
 
